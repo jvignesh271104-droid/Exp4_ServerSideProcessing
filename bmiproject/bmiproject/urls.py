01c07fbde55d28ce54d5170f6ff47bcdffe0583a
jvignesh271104-droid <jvignesh271104@gmail.com>
2025-12-19 19:58:22 +0530

    Success

---
+++ bmiproject/bmiproject/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from bmi_app import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('bmi', views.bmicalculator, name='bmi'),
+]
